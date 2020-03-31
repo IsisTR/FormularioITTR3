40cf0b6a659217a99110d03219426545f2c580af
IsisTR <62315125+IsisTR@users.noreply.github.com>
2020-03-31 19:36:17 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio</title>
+
+	
+</head>
+<body>
+	<ul>
+		<li><a href="Isis_Tovar_26834873.html">Formulario</a></li>
+		<li><a href="#"></a>Módulo 2</li>
+		<li><a href="#"></a>Módulo 3</li>
+	</ul>
+
+</body>
+</html>
